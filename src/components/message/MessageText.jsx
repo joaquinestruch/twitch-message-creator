b669fed2e60fe7084d7c7f735c8d8774cea9f6ae
joaquin estruch <joacoetruuu@gmail.com>
2023-07-30 18:55:44 -0300

    mas espacios

--- src/components/message/MessageText.jsx
+++ src/components/message/MessageText.jsx
@@ -1,10 +1,20 @@
+function MessageText({ messageText }) {
+  const formatText = (text) => {
+    const words = text.split(' ');
+    const groupedWords = [];
+    while (words.length > 0) {
+      groupedWords.push(words.splice(0, 5).join(' '));
+    }
+    return groupedWords.join('<br>');
+  };
 
-function MessageText({messageText}) {
+  const formattedText = messageText.length < 1 ? "message" : formatText(messageText);
+
   return (
-    <p style={{color:"white"}} className="message-text">
-        {messageText.length < 1 ? "message" : messageText}
+    <p style={{ color: "white" }} className="message-text">
+      <span dangerouslySetInnerHTML={{ __html: formattedText }} />
     </p>
-  )
+  );
 }
 
 export default MessageText
