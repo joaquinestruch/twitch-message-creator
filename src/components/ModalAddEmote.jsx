0250b68f19bb88b2c8207c7c2fe5d276a3e68451
joaquin estruch <joacoetruuu@gmail.com>
2023-07-31 02:59:00 -0300

    error alert

--- src/components/ModalAddEmote.jsx
+++ src/components/ModalAddEmote.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import { emblestList } from "../utils/embleds";
 import "./modal.css"
 
 const Modal = ({ isOpen, onClose }) => {
+  
+    const [isCorrect, setIsCorrect] = useState(true); 
+
     return (
       <div className={`modal ${isOpen ? 'open' : ''}`}>
          
@@ -12,18 +16,31 @@ const Modal = ({ isOpen, onClose }) => {
           }}>x  </button></h2>
          
           <form action="submit" onSubmit={(e) => {
-            e.preventDefault()
-            const linkRegex = /^(ftp|http|https):\/\/[^ "]+$/
             
-            if(linkRegex.test(e.target.elements[0].value)){
-                emblestList.push(e.target.elements[0].value)
-                onClose(false)
-                e.target.elements[0].value = "";
-                return
+            e.preventDefault();
+            const linkRegex = /^(ftp|http|https):\/\/[^ "]+$/;
+        
+            if (linkRegex.test(e.target.elements[0].value)) {
+              emblestList.push(e.target.elements[0].value);
+              onClose(false); 
+              e.target.elements[0].value = '';
+              return;
             }
         
+            setIsCorrect(false); 
+            setTimeout(() => {
+              setIsCorrect(true); 
+            }, 2500);
+
+        
           }}>
-          <input className="modal-input" type="text" placeholder="Image link" />
+          <input className="modal-input" type="text" placeholder="Image link" style={{border: "1px solid #a970ff"}} />
+
+            {
+              isCorrect === false ? <div className="alert-invalid-link-modal">Invalid Link</div> : ""
+            }
+
+          
           <button>Add</button>
           </form>
 
