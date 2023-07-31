4b71ca63eafebfd4dc2b242b49d5de90d44805e4
joaquin estruch <joacoetruuu@gmail.com>
2023-07-31 04:23:34 -0300

    .ico

--- src/components/message/Message.jsx
+++ src/components/message/Message.jsx
@@ -1,19 +1,30 @@
-import MessageText from "./MessageText"
-import UsernameMessage from "./UsernameMessage"
-function Message({username, messageText,colorUsername, embledsArray, isModalOpen, setIsModalOpen}) {
+import MessageText from "./MessageText";
+import UsernameMessage from "./UsernameMessage";
+function Message({
+  username,
+  messageText,
+  colorUsername,
+  embledsArray,
+  isModalOpen,
+  setIsModalOpen,
+}) {
   return (
     <div className="message">
-        <div className="embleds-div">
-          {
-            embledsArray?.length > 0 ? embledsArray.map((e) => {
-              return <img src={e} alt={e} key={crypto.randomUUID()} />
-            }) : ""
-          }
-        </div>
-        <UsernameMessage messageText={messageText} username={username} colorUsername={colorUsername}/>
+      <div className="embleds-div" >
+        {embledsArray?.length > 0
+          ? embledsArray.map((e) => {
+              return <img src={e} alt={e} key={crypto.randomUUID()} />;
+            })
+          : ""}
+
+      </div>
+      
+      <UsernameMessage messageText={messageText} username={username} colorUsername={colorUsername}/>
         <MessageText messageText={messageText}/>
+      
+
     </div>
-  )
+  );
 }
 
-export default Message
+export default Message;
