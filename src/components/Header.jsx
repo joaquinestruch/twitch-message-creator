d748b38b05e5814201845a26203cced14d6b9fcc
joaquinestruch <joacoetruuu@gmail.com>
2025-11-28 13:34:09 -0300

    feat: add Kick Message Generator link and associated styling.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ function Header() {
   return (
     <header>
         <h1>Twitch Message Creator</h1>
+        <a href="https://kick-message-creator.vercel.app/" target="_blank" className="kick-button">Kick Message Generator</a>
         <img src="https://blog.cdn.own3d.tv/resize=fit:crop,height:400,width:600/LpfvrIJwRweax4SlbXKq" alt="" />
     </header>
   )
