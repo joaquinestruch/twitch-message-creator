67295455c3ab603bf3b458470193f54672988406
Joaquin <joacoetruuu@gmail.com>
2025-12-01 11:05:51 -0300

    feat: Implement initial UI and styling for Twitch message creator, including a main component and embed utility.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,17 @@
 import Message from "./message/Message"
 import Nav from "./nav/Nav"
 
-function Main({isModalOpen, setIsModalOpen, username, setUsername, messageText, setMessageText, colorUsername, setColorUsername, embledsArray, setEmbledsArray}) {
+function Main({ isModalOpen, setIsModalOpen, username, setUsername, messageText, setMessageText, colorUsername, setColorUsername, embledsArray, setEmbledsArray }) {
   return (
-    <main className="container-main">
-        <Nav isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} username={username} messageText={messageText} colorUsername={colorUsername} embledsArray={embledsArray} setUsername={setUsername} setMessageText={setMessageText} setColorUsername={setColorUsername} setEmbledsArray={setEmbledsArray}/>
-        <Message isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}  username={username} messageText={messageText} colorUsername={colorUsername} embledsArray={embledsArray}/>
-    </main>
+    <>
+      <main className="container-main">
+        <Nav isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} username={username} messageText={messageText} colorUsername={colorUsername} embledsArray={embledsArray} setUsername={setUsername} setMessageText={setMessageText} setColorUsername={setColorUsername} setEmbledsArray={setEmbledsArray} />
+        <Message isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} username={username} messageText={messageText} colorUsername={colorUsername} embledsArray={embledsArray} />
+      </main>
+      <footer className="site-footer">
+        <a href="/privacy-policy">Privacy Policy</a>
+      </footer>
+    </>
   )
 }
 
